3ae44f971888c8999e7a50e0ef3394b17e3f9176
Hendrik Neuschulz <hneuhb@t-online.de>
2022-12-10 21:20:17 +0100

    just a very small fix

--- pages/recipes/[id]/index.js
+++ pages/recipes/[id]/index.js
@@ -58,30 +58,32 @@ export default function recipeDetailPage({ recipes }) {
         </StyledSection>
       </StyledMain>
 
-      <StyledFooter href={"/mealcenter"}>
-        <svg
-          id="Layer_1"
-          enableBackground="new 0 0 64 64"
-          height="30"
-          viewBox="0 0 64 64"
-          width="30"
-          xmlns="http://www.w3.org/2000/svg"
-        >
-          <g>
+      <StyledFooter>
+        <Link href={"/mealcenter"}>
+          <svg
+            id="Layer_1"
+            enableBackground="new 0 0 64 64"
+            height="30"
+            viewBox="0 0 64 64"
+            width="30"
+            xmlns="http://www.w3.org/2000/svg"
+          >
             <g>
               <g>
-                <path d="m59.5 34h-54.9c-1.1 0-2-.9-2-2s.9-2 2-2h54.9c1.1 0 2 .9 2 2s-.9 2-2 2z" />
+                <g>
+                  <path d="m59.5 34h-54.9c-1.1 0-2-.9-2-2s.9-2 2-2h54.9c1.1 0 2 .9 2 2s-.9 2-2 2z" />
+                </g>
               </g>
             </g>
-          </g>
-          <g>
             <g>
               <g>
-                <path d="m26.5 56c-.5 0-1-.2-1.4-.6l-22-22c-.8-.8-.8-2 0-2.8l22-22c.8-.8 2-.8 2.8 0s.8 2 0 2.8l-20.5 20.6 20.6 20.6c.8.8.8 2 0 2.8-.4.4-.9.6-1.5.6z" />
+                <g>
+                  <path d="m26.5 56c-.5 0-1-.2-1.4-.6l-22-22c-.8-.8-.8-2 0-2.8l22-22c.8-.8 2-.8 2.8 0s.8 2 0 2.8l-20.5 20.6 20.6 20.6c.8.8.8 2 0 2.8-.4.4-.9.6-1.5.6z" />
+                </g>
               </g>
             </g>
-          </g>
-        </svg>
+          </svg>
+        </Link>
         <Link href={`/recipes/${id}/comments`}>
           <svg
             id="Layer"
